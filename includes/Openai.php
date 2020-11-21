9ae724ac010c560de39d2119cde8047d5cc597d9
Luis Paulo Dobreira <64305063+ldobreira@users.noreply.github.com>
2020-11-22 00:37:55 +0700

    small update

--- includes/Openai.php
+++ includes/Openai.php
@@ -2,7 +2,7 @@
 class Openai
 {
    private function secretKey(){
-       return $secret_key = 'Bearer ******YOUR-KEY-HERE********';
+    return $secret_key = 'Bearer ******YOUR-KEY-HERE********';
    }
 
    public function request($engine, $prompt, $max_tokens){ 
@@ -21,16 +21,16 @@ class Openai
     $postfields = json_encode($request_body);
     $curl = curl_init();
     curl_setopt_array($curl, [
-	CURLOPT_URL => "https://api.openai.com/v1/engines/" . $engine . "/completions",
-	CURLOPT_RETURNTRANSFER => true,
-	CURLOPT_FOLLOWLOCATION => true,
-	CURLOPT_ENCODING => "",
-	CURLOPT_MAXREDIRS => 10,
-	CURLOPT_TIMEOUT => 30,
+    CURLOPT_URL => "https://api.openai.com/v1/engines/" . $engine . "/completions",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
     CURLOPT_CUSTOMREQUEST => "POST",
     CURLOPT_POSTFIELDS => $postfields,
-	CURLOPT_HTTPHEADER => [
+    CURLOPT_HTTPHEADER => [
         'Content-Type: application/json',
         'Authorization: ' . $this->secretKey()
     ],
